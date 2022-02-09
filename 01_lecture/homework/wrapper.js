b8ded4034e7c912f745d1e280c4454b1892049cf
VasylKhrapko <khrapko2002@gmail.com>
2022-02-09 19:36:14 +0200

    Added wrapper function for caching functions results

--- 01_lecture/homework/wrapper.js
+++ 01_lecture/homework/wrapper.js
@@ -3,4 +3,37 @@
 // 2. Write a function-wrapper, that will
 // cache the result of any other function.
 
-const wrapper = args => () => {};
+const generateKey = args => {
+  return args.map(arg => `${arg.toString()}:${typeof arg}`).join('|');
+};
+
+const wrapper = fn => {
+  const cache = new Object(null);
+  return (...args) => {
+    const key = generateKey(args);
+    const value = cache[key];
+    if (value) {
+      // console.log('from cache'); // for tests
+      return value;
+    }
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  };
+};
+
+//tests
+
+const add = (a, b) => a + b;
+const pow = (x, n) => (n === 1 ? x : x * pow(x, n - 1));
+
+const mAdd = wrapper(add);
+const mPow = wrapper(pow);
+
+mAdd(5, 13); // 18
+mAdd(5, 13); // 18 from cache
+mAdd(2, 23); // 25
+mAdd(2, 23); // 25 from cache
+
+mPow(3, 15); // 14348907
+mPow(3, 15); // 14348907 from cache
