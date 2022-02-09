ff272e0939c3e38ae705ce4e3dd32b385898cc5c
VasylKhrapko <khrapko2002@gmail.com>
2022-02-09 18:07:41 +0200

    Added function for deep object cloning (task 1)

---
+++ 01_lecture/homework/deepClone.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// 1. Write the clone function so that it can
+// clone deeply the object passed as a parameter.
+
+const deepClone = obj => {
+  if (!obj || typeof obj !== 'object') {
+    return obj;
+  }
+
+  let newObj;
+  if (Array.isArray(obj)) {
+    newObj = [];
+    for (const value of obj) {
+      newObj.push(deepClone(value));
+    }
+  } else {
+    newObj = {};
+    const keys = Object.keys(obj);
+
+    for (const key of keys) {
+      newObj[key] = deepClone(obj[key]);
+    }
+  }
+  return newObj;
+};
+
+const testObj = {
+  ex1: {
+    ex11: 'str1',
+    ex12: 12,
+  },
+  ex2: {
+    ex21: [1, '2', [3, 4]],
+  },
+  exMethodLog(value) {
+    console.log(value);
+  },
+};
+
+const newObj = deepClone(testObj);
+
+console.log(newObj === testObj); //false
+console.log(newObj.ex1 === testObj.ex1); //false
+console.log(newObj.ex2 === testObj.ex2); //false
+console.log(newObj.ex2.ex21 === testObj.ex2.ex21); //false
+console.log(newObj.ex2.ex21[2] === testObj.ex2.ex21[2]); // false
---
+++ 01_lecture/homework/wrapper.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// 2. Write a function-wrapper, that will
+// cache the result of any other function.
+
+const wrapper = args => () => {};
