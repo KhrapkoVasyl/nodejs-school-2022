64ce23e199d098095ea19b8e5e34e1c1824355ce
VasylKhrapko <khrapko2002@gmail.com>
2022-02-15 23:07:03 +0200

    Added implementation in TypeScript for the function searchSerial from HW1

---
+++ 02_lecture/src/homework/hw1InTypeScript/searchSerial.ts
@@ -0,0 +1,24 @@
+// 3. Create a function searchSerial, which takes two
+// parameters: 1 - an array of string or numeric values,
+// 2 - searched value. The function should return a
+// number - maximum count of serial entries of the given value.
+
+const searchSerial = (
+  arr: number[] | string[],
+  value: number | string
+): number => {
+  let maxSeries: number = 0;
+  let currentSeries: number = 0;
+
+  for (const el of arr) {
+    el === value ? currentSeries++ : (currentSeries = 0);
+    if (currentSeries > maxSeries) maxSeries = currentSeries;
+  }
+
+  return maxSeries;
+};
+
+console.log(searchSerial([1, 1, 1, 2, 2, 2, 3, 1, 1, 3], 1)); // 3
+console.log(searchSerial(['fizzbuzz', 'fizz', 'fizz', 'buzz', 'buzz'], 'fizz')); // 2
+console.log(searchSerial([1, 1, 1], 2)); // 0
+console.log(searchSerial([1, 1, 1, 2, 2, 2, 1, 1, 1, 1], 1)); // 4
