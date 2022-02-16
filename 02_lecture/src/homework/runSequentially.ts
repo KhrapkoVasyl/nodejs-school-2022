e2528076c3a1c1aa1aa4000f79ed5b6bfbf0379b
VasylKhrapko <khrapko2002@gmail.com>
2022-02-16 16:22:36 +0200

    Added function runSequentially (HW2, task 1)

---
+++ 02_lecture/src/homework/runSequentially.ts
@@ -0,0 +1,40 @@
+// 1. Write a function that accepts any kind of array and an asynchronous callback,
+// that is invoked on each array element sequentially.
+// The result of invocation must be an array of callback results.
+// All types must apply automatically (Template function).
+
+async function runSequentially<Type>(
+  array: Type[],
+  callback: Function
+): Promise<{ item: Type; index: number }[]> {
+  const callbackResults: Array<{ item: Type; index: number }> = [];
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    // console.log(`Start with item: ${item}; index: ${i}`); // for sequential execution test
+    const result = await callback(item, i);
+    // console.log(`End with item: ${item}; index: ${i}`); // for sequential execution test
+    callbackResults.push(result);
+  }
+  return callbackResults;
+}
+
+// test
+
+(async () => {
+  const array: Array<string> = ['one', 'two', 'three', 'four', 'five'];
+
+  const results = await runSequentially<string>(
+    array,
+    (item: string, index: number) =>
+      Promise.resolve({
+        item,
+        index,
+      })
+  );
+
+  console.log(results); /* [ { item: 'one', index: 0 },
+                             { item: 'two', index: 1 },
+                             { item: 'three', index: 2 }
+                             { item: 'four', index: 3 },
+                             { item: 'five', index: 4 } */
+})();
