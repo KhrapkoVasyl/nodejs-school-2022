b74ef290d5b2142770215d1a5a9318bb2fbc5c3b
VasylKhrapko <khrapko2002@gmail.com>
2022-03-19 20:50:28 +0200

    Added .env config and jwt dependency to protect with a token endpoints for updating and deleting a user

--- 05_lecture/homework/src/app.module.ts
+++ 05_lecture/homework/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- 05_lecture/homework/src/users/users.module.ts
+++ 05_lecture/homework/src/users/users.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  imports: [
+    JwtModule.register({
+      secret: process.env.SECRET_KEY || 'SECRET_KEY',
+      signOptions: {
+        expiresIn: '48h',
+      },
+    }),
+  ],
 })
 export class UsersModule {}
